c7f8540ebf848d1cc3a94bfdede250b464ffc726
GH0S7 <gh0s7@GH0S7.Home>
2024-10-26 22:01:35 +0100

    creating the game function

--- tic_tac_toe/src/main/java/com/tic_tac_toe/Main.java
+++ tic_tac_toe/src/main/java/com/tic_tac_toe/Main.java
@@ -6,7 +6,21 @@ public class Main {
     public static void main(String[] args) {
         char[] tableline = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
         Scanner input = new Scanner(System.in);
-        while(true) { 
+        boolean winnerbool = false;
+        while(!winnerbool) { 
+            game(1, tableline, input);
+            game(2, tableline, input);
+            }
+        }
+
+        public static int inputBox(Scanner input, String msg) {
+            System.out.print(msg);
+            int num = input.nextInt();
+            return num;
+        }
+
+        public static void game(int player, char[] tableline, Scanner input) {
+            char playerchar = (player == 1)?'O':'X';
             System.out.println("Tic Tac Toe (Player 1): \n1: "+"["+tableline[0]+"]"+"["+tableline[1]+"]"+"["+tableline[2]+"]"+"\n"+
             "2: "+"["+tableline[3]+"]"+"["+tableline[4]+"]"+"["+tableline[5]+"]"+"\n"+
             "3: "+"["+tableline[6]+"]"+"["+tableline[7]+"]"+"["+tableline[8]+"]"+"\n");
@@ -16,26 +30,8 @@ public class Main {
                 System.out.println("error, input incorrect");
                 number = inputBox(input, "number (from 0 to 8 in line): "); 
             } 
-            tableline[number] = 'O';
+            tableline[number] = playerchar;
 
-
-            System.out.println("Tic Tac Toe (Player 2): \n1: "+"["+tableline[0]+"]"+"["+tableline[1]+"]"+"["+tableline[2]+"]"+"\n"+
-            "2: "+"["+tableline[3]+"]"+"["+tableline[4]+"]"+"["+tableline[5]+"]"+"\n"+
-            "3: "+"["+tableline[6]+"]"+"["+tableline[7]+"]"+"["+tableline[8]+"]"+"\n");
-            int numberX = inputBox(input, "line: ");
-
-
-
-            while( numberX < 0 && numberX > 8 || tableline[number] == 'O' || tableline[number] == 'X') {
-                System.out.println("error, input incorrect");
-            } 
-            tableline[numberX] = 'X';
-             
-            }
-        }
-        public static int inputBox(Scanner input, String msg) {
-            System.out.print(msg);
-            int num = input.nextInt();
-            return num;
+            
         }
 }
