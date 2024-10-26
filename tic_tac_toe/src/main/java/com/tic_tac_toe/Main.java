82b216bf727120383db6069e9cd3b31faacea543
GH0S7 <gh0s7@GH0S7.Home>
2024-10-26 22:55:28 +0100

    V1 criado

--- tic_tac_toe/src/main/java/com/tic_tac_toe/Main.java
+++ tic_tac_toe/src/main/java/com/tic_tac_toe/Main.java
@@ -4,34 +4,71 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        char[] tableline = {' ',' ',' ',' ',' ',' ',' ',' ',' '};
+        char[] tableline = {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '};
         Scanner input = new Scanner(System.in);
-        boolean winnerbool = false;
-        while(!winnerbool) { 
+        int winner = 0;
+
+        while (winner == 0) {
             game(1, tableline, input);
+            winner = winner(tableline);
+            if (winner != 0) break;  
+            
             game(2, tableline, input);
+            winner = winner(tableline);
+        }
+
+        System.out.println("Winner: player " + winner);
+    }
+
+    public static int winner(char[] tableline) {
+        
+        for (int i = 0; i < 3; i++) {
+            if (tableline[i * 3] == 'X' && tableline[i * 3 + 1] == 'X' && tableline[i * 3 + 2] == 'X') {
+                return 2;  
+            }
+            if (tableline[i * 3] == 'O' && tableline[i * 3 + 1] == 'O' && tableline[i * 3 + 2] == 'O') {
+                return 1;  
             }
         }
 
-        public static int inputBox(Scanner input, String msg) {
-            System.out.print(msg);
-            int num = input.nextInt();
-            return num;
+        
+        for (int i = 0; i < 3; i++) {
+            if (tableline[i] == 'X' && tableline[i + 3] == 'X' && tableline[i + 6] == 'X') {
+                return 2;
+            }
+            if (tableline[i] == 'O' && tableline[i + 3] == 'O' && tableline[i + 6] == 'O') {
+                return 1;
+            }
         }
 
-        public static void game(int player, char[] tableline, Scanner input) {
-            char playerchar = (player == 1)?'O':'X';
-            System.out.println("Tic Tac Toe (Player 1): \n1: "+"["+tableline[0]+"]"+"["+tableline[1]+"]"+"["+tableline[2]+"]"+"\n"+
-            "2: "+"["+tableline[3]+"]"+"["+tableline[4]+"]"+"["+tableline[5]+"]"+"\n"+
-            "3: "+"["+tableline[6]+"]"+"["+tableline[7]+"]"+"["+tableline[8]+"]"+"\n");
-            int number = inputBox(input, "number (from 0 to 8 in line): "); 
+        
+        if (tableline[0] == 'X' && tableline[4] == 'X' && tableline[8] == 'X') return 2;
+        if (tableline[2] == 'X' && tableline[4] == 'X' && tableline[6] == 'X') return 2;
+        if (tableline[0] == 'O' && tableline[4] == 'O' && tableline[8] == 'O') return 1;
+        if (tableline[2] == 'O' && tableline[4] == 'O' && tableline[6] == 'O') return 1;
 
-            while( number < 0 && number > 8 || tableline[number] == 'O' || tableline[number] == 'X') {
-                System.out.println("error, input incorrect");
-                number = inputBox(input, "number (from 0 to 8 in line): "); 
-            } 
-            tableline[number] = playerchar;
+        return 0; 
+    }
 
-            
+    public static int inputBox(Scanner input, String msg) {
+        System.out.print(msg);
+        return input.nextInt();
+    }
+
+    public static void game(int player, char[] tableline, Scanner input) {
+        char playerchar = (player == 1) ? 'O' : 'X';
+        System.out.println("Tic Tac Toe (Player " + player + "):");
+        System.out.println("1: [" + tableline[0] + "][" + tableline[1] + "][" + tableline[2] + "]");
+        System.out.println("2: [" + tableline[3] + "][" + tableline[4] + "][" + tableline[5] + "]");
+        System.out.println("3: [" + tableline[6] + "][" + tableline[7] + "][" + tableline[8] + "]");
+
+        int number = inputBox(input, "Number (from 0 to 8): ");
+
+        while (number < 0 || number > 8 || tableline[number] == 'O' || tableline[number] == 'X') {
+            System.out.println("Error, input incorrect");
+            number = inputBox(input, "Number (from 0 to 8): ");
         }
+
+        tableline[number] = playerchar;
+    }
 }
